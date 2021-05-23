cbc1dda63b4bd1866df1288bfc09145be6e5abda
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-23 04:28:26 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kaffoux on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
